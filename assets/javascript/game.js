57af9c317ef280b7678e80619d652b4bcd070d04
Chiefcos <41163851+Chiefcos@users.noreply.github.com>
2019-04-01 16:05:38 -0700

    made some changes to the game, reduced my code by half

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-  alert("I'm working");
-
+  //   Here we initialize all the scores for the game
   var wins = 0;
   var losses = 0;
   var crystalRandomNumber = [];
@@ -11,7 +10,10 @@ $(document).ready(function() {
   var randomNumber = [];
   var totalScore = 0;
   var emptyShell;
+  var input;
+  var check = false;
 
+  //   Here we initialize a function to start the game whenever the function is called
   function initializeGame() {
     crystalOne;
     crystalTwo;
@@ -19,134 +21,72 @@ $(document).ready(function() {
     crystalFour;
     crystalRandomNumber = [];
     randomNumber = [];
-    TotalScore;
+    TotalScore = 0;
+    check = false;
+
+    // We create a random number between 19 en 120
+    randomNumber = Math.floor(Math.random() * 101) + 19;
+    console.log("i'm randomNumber", randomNumber);
   }
 
-  randomNumber = Math.floor(Math.random() * 101) + 19;
-  console.log("i'm randomNumber", randomNumber);
+  // We initialize the game
+  initializeGame();
 
+  //   We create a random number between 2 and 13, which we do 4 times over. One for every crystal
   for (let i = 0; i < 4; i++) {
     emptyShell = Math.floor(Math.random() * 11) + 2;
     console.log("i am emptyshell", emptyShell);
     crystalRandomNumber.push(emptyShell);
   }
-
   console.log("I am crystalRandomNumber", crystalRandomNumber);
-
+  // Here we assign everyone of the random numbers to the 4 crystals
   crystalOne = crystalRandomNumber[0];
   crystalTwo = crystalRandomNumber[1];
   crystalThree = crystalRandomNumber[2];
   crystalFour = crystalRandomNumber[3];
 
-  //   crystalOne = $("#crystal-one").val;
-  //   $("#crystal-two").append(crystalTwo);
-  //   $("#crystal-three").append(crystalThree);
-  //   $("#crystal-four").append(crystalFour);
+  $("#crystal-one").val(crystalOne);
+  $("#crystal-two").val(crystalTwo);
+  $("#crystal-three").val(crystalThree);
+  $("#crystal-four").val(crystalFour);
 
-  //   console.log("I am crystal one", $("#crystal-one").val;
-  console.log("i am crystaltwo", crystalTwo);
-  console.log("i am crystalthree", crystalThree);
-  console.log("i am crystalfour", crystalFour);
+  //   Here we check if the value is actually appended to to crystals
+  console.log("i am the actual one", $("#crystal-one").val());
+  console.log("i am the actual one", $("#crystal-two").val());
+  console.log("i am the actual one", $("#crystal-three").val());
+  console.log("i am the actual one", $("#crystal-four").val());
 
+  //   We print the beginning scores onto the screen
   $("#random-number").html(randomNumber);
-  $("#your-score").html(totalScore);
   $("#wins").html(wins);
   $("#losses").html(losses);
 
-  //   $(".crystal").on("click", function() {
-  //     totalScore += $(this).val;
-  //     console.log("I'm the total score after clicking", totalScore);
-  //   });
-
-  $("#crystal-one").on("click", function() {
-    totalScore += crystalOne;
-    console.log(totalScore);
+  //  This is the function that actually runs the game.
+  //  We put the value of the crystals onto the totalscore
+  $(".crystal").on("click", function() {
+    input = parseInt($(this).val());
+    totalScore += input;
     $("#your-score").html(totalScore);
+
+    // if our totalscore matches the random number we created this will show that we have won.
     if (randomNumber === totalScore) {
       wins = wins + 1;
+      check = true;
       $("#wins").html(wins);
       console.log(totalScore);
       alert("Im working: You win!");
-      initializeGame();
     }
+
+    // if our totalscore doesn't match what the random number is than we lose
     if (randomNumber < totalScore) {
       losses = losses + 1;
       $("#losses").html(losses);
       alert("Im working: You Lose!");
+      check = true;
+    }
+    // Here we reset the game
+    if (check) {
       initializeGame();
     }
   });
-
-  $("#crystal-two").on("click", function() {
-    totalScore += crystalTwo;
-    console.log(totalScore);
-    $("#your-score").html(totalScore);
-    if (randomNumber === totalScore) {
-      wins = wins + 1;
-      $("#wins").html(wins);
-      console.log(totalScore);
-      alert("Im working: You win!");
-      initializeGame();
-    }
-    if (randomNumber < totalScore) {
-      losses = losses + 1;
-      $("#losses").html(losses);
-      alert("Im working: You Loss!");
-      initializeGame();
-    }
-  });
-
-  $("#crystal-three").on("click", function() {
-    totalScore += crystalThree;
-    console.log(totalScore);
-    $("#your-score").html(totalScore);
-    if (randomNumber === totalScore) {
-      wins = wins + 1;
-      $("#wins").html(wins);
-      console.log(totalScore);
-      alert("Im working: You win!");
-      initializeGame();
-      console.log(initializeGame);
-    }
-    if (randomNumber < totalScore) {
-      losses = losses + 1;
-      alert("Im working: You Loss!");
-      $("#losses").html(losses);
-      initializeGame();
-      console.log(initializeGame);
-    }
-  });
-
-  $("#crystal-four").on("click", function() {
-    totalScore += crystalFour;
-    console.log(totalScore);
-    $("#your-score").html(totalScore);
-    if (randomNumber === totalScore) {
-      wins = wins + 1;
-      $("#wins").html(wins);
-      console.log(totalScore);
-      alert("Im working: You win!");
-      initializeGame();
-    }
-    if (randomNumber < totalScore) {
-      losses = losses + 1;
-      $("#losses").html(losses);
-      alert("Im working: You Loss!");
-      initializeGame();
-    }
-  });
-
-  console.log("totalscore is a ", typeof totalScore);
-  console.log("randomnumber is a ", typeof randomNumber);
-
-  //   if ($("#random-number") === $("#your-score")) {
-  //     wins = wins + 1;
-  //     $("#wins").html(wins);
-  //     console.log(totalScore);
-  //     alert("Im working: You win!");
-  //   }
-  //   if (randomNumber < totalScore) {
-  //     losses = losses + 1;
-  //     alert("Im working: You Loss!");
-  //   }
 });
